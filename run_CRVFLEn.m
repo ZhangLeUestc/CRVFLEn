71902dc322673497e02c1bef24205f009047f848
Zhang Le <zhangleuestc@gmail.com>
2016-07-10 11:18:46 +0800

    Update run_CRVFLEn.m

--- run_CRVFLEn.m
+++ run_CRVFLEn.m
@@ -1,5 +1,5 @@
 function results=run_CRVFLEn(seq, res_path, bSaveImage)
 
 close all;
-results = MEEMTrack_v1(seq.path, seq.ext, false, seq.init_rect, seq.startFrame,seq.endFrame);
+results = CRVFLEn(seq.path, seq.ext, false, seq.init_rect, seq.startFrame,seq.endFrame);
 end
